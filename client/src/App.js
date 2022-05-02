2137c8b72d41971cef3eda23bbe3743023d04195
Gaby Pineda <gabygabypineda@gmail.com>
2022-05-02 19:49:53 +0200

    adds boostrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => (
+  <div className="container mt-4">
+    <h4 className="display-4 text-center mb-4">
+      <i className="fab fa-react" /> Create your meme!
+    </h4>
+  </div>
+);
 
 export default App;
