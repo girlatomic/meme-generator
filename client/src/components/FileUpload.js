2f18821a6ae30c720d3cd82ad2ea74d190851617
Gaby Pineda <gabygabypineda@gmail.com>
2022-05-03 17:13:34 +0200

    adds upload progress bar

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -1,51 +1,21 @@
 import React, { Fragment, useState } from "react";
 import axios from "axios";
 import Message from "./Message";
+import Progress from "./Progress";
 
 const FileUpload = () => {
   const [file, setFile] = useState("");
   const [filename, setFilename] = useState("Choose File");
   const [uploadedFile, setUploadedFile] = useState({});
   const [message, setMessage] = useState("");
+  const [uploadPercentage, setUploadPercentage] = useState(0);
 
   const onChange = (e) => {
     setFile(e.target.files[0]);
     setFilename(e.target.files[0].name);
   };
 
-  // async function uploadFile(formData) {
-  //   try {
-  //     const res = await axios.post("/upload", formData, {
-  //       headers: {
-  //         "Content-Type": "multipart/form-data",
-  //       },
-  //     });
-
-  //     const { fileName, filePath } = res.data;
-  //     console.log("dat", res.data);
-
-  //     setUploadedFile({ fileName, filePath });
-  //   } catch (err) {
-  //     if (err.response.status === 500) {
-  //       setMessage("There was a problem with the server");
-  //     } else {
-  //       setMessage(err.response.data.msg);
-  //     }
-  //   }
-  // }
-
-  // const onSubmit = (e) => {
-  //   e.preventDefault();
-  //   const formData = new FormData();
-  //   formData.append("file", file);
-  //   uploadFile(formData);
-
-  //   setFile(null);
-  //   e.target.reset();
-  // };
-
-  const onSubmit = async (e) => {
-    e.preventDefault();
+  async function uploadFile() {
     const formData = new FormData();
     formData.append("file", file);
     try {
@@ -53,9 +23,16 @@ const FileUpload = () => {
         headers: {
           "Content-Type": "multipart/form-data",
         },
+        onUploadProgress: (progressEvent) => {
+          setUploadPercentage(
+            parseInt(
+              Math.round((progressEvent.loaded * 100) / progressEvent.total)
+            )
+          );
+        },
       });
 
-      // Clear percentage
+      setTimeout(() => setUploadPercentage(0), 10000);
 
       const { fileName, filePath } = res.data;
 
@@ -68,14 +45,50 @@ const FileUpload = () => {
       } else {
         setMessage(err.response.data.msg);
       }
+      setUploadPercentage(0);
     }
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    uploadFile();
+
+    setFile(null);
+    e.target.reset();
   };
+
+  // const onSubmit = async (e) => {
+  //   e.preventDefault();
+  //   const formData = new FormData();
+  //   formData.append("file", file);
+  //   try {
+  //     const res = await axios.post("/upload", formData, {
+  //       headers: {
+  //         "Content-Type": "multipart/form-data",
+  //       },
+  //     });
+
+  //     // Clear percentage
+
+  //     const { fileName, filePath } = res.data;
+
+  //     setUploadedFile({ fileName, filePath });
+
+  //     setMessage("File Uploaded");
+  //   } catch (err) {
+  //     if (err.response.status === 500) {
+  //       setMessage("There was a problem with the server");
+  //     } else {
+  //       setMessage(err.response.data.msg);
+  //     }
+  //   }
+  // };
 
   return (
     <Fragment>
       {message ? <Message msg={message} /> : null}
       <form onSubmit={onSubmit}>
-        <div className="input-group">
+        <div className="input-group mb-2">
           <input
             type="file"
             className="form-control"
@@ -93,11 +106,11 @@ const FileUpload = () => {
             Upload
           </button>
         </div>
+        <Progress percentage={uploadPercentage} />
       </form>
       {uploadedFile && (
         <div className="row mt-5">
           <div className="col-md-6 m-auto">
-            <h3>{uploadedFile.fileName}</h3>
             <img style={{ width: "100%" }} src={uploadedFile.filePath} alt="" />
           </div>
         </div>
