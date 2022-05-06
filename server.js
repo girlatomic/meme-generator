b3499db90ef630bfc91760b348242d3de33ddea5
Gaby Pineda <gabygabypineda@gmail.com>
2022-05-06 17:59:22 +0200

    modifies start

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(
   })
 );
 
-app.use(express.static(path.join(__dirname, "/client/build")));
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 
 // Upload Endpoint
 app.post("/upload", (req, res) => {
